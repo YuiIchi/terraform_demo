e0b8c19980fd0477a17aafdff0d1427e7ec595a3
YuiIchi <yuiichi@live.cn>
2019-09-17 16:26:44 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,9 +2,9 @@ provider "huaweicloud" {
   #agency_name        = "${var.agency_name}"
   #agency_domain_name = "${var.agency_domain_name}"
   #delegated_project  = "${var.delegated_project}"
-  access_key         = "${var.access_key}"
-  secret_key         = "${var.secret_key}"
+  access_key         = var.access_key
+  secret_key         = var.secret_key
   #domain_name        = "${var.domain_name}"
-  region             = "${var.region}"
-  auth_url           = "${var.auth_url}"
+  region             = var.region
+  auth_url           = var.auth_url
 }
