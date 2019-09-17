88cdf527b45fc6a4efe2213b33e86721b2689e09
YuiIchi <yuiichi@live.cn>
2019-09-17 15:22:09 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "http" "example" {
   url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
 
   # Optional request headers
-  request_headers {
-    Accept = "application/json"
-  }
+  #request_headers {
+  #  Accept = "application/json"
+  #}
 }
