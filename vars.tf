e6d82fcdc230ff455b41b4d773da278e30ac5473
YuiIchi <yuiichi@live.cn>
2019-09-17 16:39:17 +0800

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,19 @@
+variable "access_key" {
+    type = "string"
+    default = "05BCTJIXSTVES287E2IL"
+}
+
+variable "secret_key" {
+    type = "string"
+    default = "vthc4vRfgPUpTo3xpbtmuaW4oR4pylLnQurBq24e"
+}
+
+variable "region" {
+    type = "string"
+    default = "cn-east-2"
+}
+
+variable "auth_url" {
+    type = "string"
+    default = "https://obs.cn-east-2.myhwclouds.com"
+}
