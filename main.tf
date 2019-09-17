db245438360e28ac65c611338e5189fdf70157ed
YuiIchi <yuiichi@live.cn>
2019-09-17 15:25:30 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
-data "http" "example" {
-  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
-
-  # Optional request headers
-  #request_headers {
-  #  Accept = "application/json"
-  #}
+# Configure the Docker provider
+provider "docker" {
+  host = "tcp://127.0.0.1:2376/"
 }
+
+resource "docker_image" "ubuntu" {
+  name = "ubuntu:latest"
+}
+
+# Create a container
+resource "docker_container" "foo" {
+  image = "${docker_image.ubuntu.latest}"
+  name  = "foo"
+}
+
